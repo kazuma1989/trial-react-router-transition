7c180460fe61455188d055b0284823ca5addda91
kazuma1989 <kazuma.ebina@quipper.com>
2023-06-30 16:02:16 +0900

    UNSAFE_NavigationContextを使うものの、history.pushにフックしてstartViewTransitionを差し込めた
    これでもまだhistory backでのtransitionは起こせていない

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React, { useContext, useMemo } from 'react'
 import ReactDOM from 'react-dom/client'
 import {
   BrowserRouter,
-  LinkProps,
+  Link,
+  UNSAFE_NavigationContext as NavigationContext,
   Route,
   Routes,
-  useHref,
-  useLinkClickHandler,
 } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
@@ -14,67 +13,90 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/a"
-          element={
-            <Layout>
-              <h1>/a</h1>
-            </Layout>
-          }
-        />
+      <MyRouter>
+        <Routes>
+          <Route
+            path="/a"
+            element={
+              <Layout>
+                <h1>/a</h1>
+              </Layout>
+            }
+          />
 
-        <Route
-          path="/b"
-          element={
-            <Layout>
-              <h1>/b</h1>
-            </Layout>
-          }
-        />
+          <Route
+            path="/b"
+            element={
+              <Layout>
+                <h1>/b</h1>
+              </Layout>
+            }
+          />
 
-        <Route
-          path="*"
-          element={
-            <Layout>
-              <App />
-            </Layout>
-          }
-        />
-      </Routes>
+          <Route
+            path="*"
+            element={
+              <Layout>
+                <App />
+              </Layout>
+            }
+          />
+        </Routes>
+      </MyRouter>
     </BrowserRouter>
   </React.StrictMode>
 )
 
-const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
-  ({ onClick, replace = false, state, target, to, ...rest }, ref) => {
-    const href = useHref(to)
-    const handleClick = useLinkClickHandler(to, {
-      replace,
-      state,
-      target,
+// // これはうまくいかない
+// const pushState = window.history.pushState.bind(window.history)
+
+// window.history.pushState = (...args) => {
+//   // @ts-expect-error startViewTransition を実験的に使いたい
+//   document.startViewTransition(() => {
+//     pushState(...args)
+//   })
+// }
+
+function MyRouter({ children }: { children: React.ReactNode }) {
+  const original = useContext(NavigationContext)
+
+  const hookedNavigation = useMemo(() => {
+    // @ts-expect-error startViewTransition を実験的に使いたい
+    if (typeof document.startViewTransition !== 'function') {
+      return original
+    }
+
+    const navigator = new Proxy(original.navigator, {
+      get(target, propertyKey: keyof typeof original.navigator, receiver) {
+        if (propertyKey !== 'push') {
+          return Reflect.get(target, propertyKey, receiver)
+        }
+
+        const originalPush = Reflect.get(target, propertyKey, receiver)
+
+        const newPush: typeof originalPush = (...args) => {
+          // @ts-expect-error startViewTransition を実験的に使いたい
+          document.startViewTransition(() => {
+            originalPush(...args)
+          })
+        }
+
+        return newPush
+      },
     })
 
-    return (
-      <a
-        {...rest}
-        href={href}
-        onClick={(event) => {
-          onClick?.(event)
-          if (!event.defaultPrevented) {
-            event.preventDefault()
-            // @ts-expect-error startViewTransition を実験的に使いたい
-            document.startViewTransition(() => {
-              handleClick(event)
-            })
-          }
-        }}
-        ref={ref}
-        target={target}
-      />
-    )
-  }
-)
+    return {
+      ...original,
+      navigator,
+    }
+  }, [original])
+
+  return (
+    <NavigationContext.Provider value={hookedNavigation}>
+      {children}
+    </NavigationContext.Provider>
+  )
+}
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
