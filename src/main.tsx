2171e84b8e631a367a6cb930a1c078785b2f5ba6
kazuma1989 <kazuma.ebina@quipper.com>
2023-06-30 14:15:45 +0900

    Linkを追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,62 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/a"
+          element={
+            <Layout>
+              <h1>/a</h1>
+            </Layout>
+          }
+        />
+
+        <Route
+          path="/b"
+          element={
+            <Layout>
+              <h1>/b</h1>
+            </Layout>
+          }
+        />
+
+        <Route
+          path="*"
+          element={
+            <Layout>
+              <App />
+            </Layout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">/</Link>
+          </li>
+          <li>
+            <Link to="/a">/a</Link>
+          </li>
+          <li>
+            <Link to="/b">/b</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {children}
+    </div>
+  )
+}
